0ea136c2930e9d180ef4baef03ac8ebfcfde7773
Alex Guerrero <alexitowy@gmail.com>
2024-09-16 17:09:37 +0200

    feat: environments, firebase hosting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,28 +2,27 @@ import { Component } from '@angular/core';
 import { MessageService, PrimeNGConfig } from 'primeng/api';
 import { EventService } from './core/services/event.service';
 import { MessageToast } from './models/enums/messageToast';
+import { environment } from '../environments/environment';
+import packageJson from '../../package.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  constructor(private primengConfig: PrimeNGConfig,
+  constructor(
+    private primengConfig: PrimeNGConfig,
     private messageService: MessageService,
-    private eventService: EventService) { 
-      this.eventService.toast$.subscribe(
-        (msg: MessageToast) => {
-          this.messageService.add(msg);
-        }
-      )
-    }
+    private eventService: EventService
+  ) {
+    this.eventService.toast$.subscribe((msg: MessageToast) => {
+      this.messageService.add(msg);
+    });
+    console.log(`environment ${environment.name} V ${packageJson.version}`);
+  }
 
-    ngOnInit() {
-        this.primengConfig.ripple = true;
-    }
-
-    
-  
-    
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+  }
 }
